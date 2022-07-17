7c13bfaa18ca6e67fd0192412c965704ee43bff6
dunglt-iks <rocketvn@gmail.com>
2022-07-17 16:46:21 +0700

    First commit for eureka

---
+++ src/test/java/com/inn/iks/eureka/IksEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inn.iks.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IksEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
